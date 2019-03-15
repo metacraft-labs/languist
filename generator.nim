434676ab506071b1e0fcd8f0570290698323b9d3
Alexander Ivanov <alehander42@gmail.com>
2019-03-15 14:19:25 +0200

    Generator rewrites

--- generator.nim
+++ generator.nim
@@ -702,29 +702,8 @@ proc generateNode(generator: Generator, node: Node): PNode =
     result = emptyNode
 
 var rewriteGenerator = initTable[string, proc(generator: Generator): PNode]()
-rewriteGenerator["lib/rubocop/cop"] = proc(generator: Generator): PNode =
-  nkStmtList.newTree(
-    generator.top,
-    nkCommand.newTree(
-      generateIdent("cop"),
-      nkStmtList.newTree(
-        generator.types,
-        generator.global,
-        generator.methods,
-        generator.main)))
 
-rewriteGenerator["spec/rubocop/cop"] = proc(generator: Generator): PNode =
-  let name = generator.module.path.splitFile[1].split("_spec")[0]
-  generator.top.add(nkImportStmt.newTree(
-    generateDirectIdent(name),
-    generateDirectIdent("test_tools")))
-
-  nkStmtList.newTree(
-    generator.top,
-    generator.types,
-    generator.global,
-    generator.methods,
-    generator.main)
+include generator_rewrite
 
 proc generate*(generator: Generator, module: Module): string =
   generator.module = module
